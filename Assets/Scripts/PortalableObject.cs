dc64e01fbedb3298f7f3d051e31f9eab26665bd9
munij-dev <muniainjuan@icloud.com>
2023-02-10 17:45:23 -0600

    Modify portable object script
    Removed unnecessary logic for recursive object cloning.

--- Assets/Scripts/PortalableObject.cs
+++ Assets/Scripts/PortalableObject.cs
@@ -10,8 +10,6 @@ public class PortalableObject : MonoBehaviour
 {
     private GameObject cloneObject;
 
-    private int inPortalCount = 0;
-    
     private Portal inPortal;
     private Portal outPortal;
 
@@ -22,45 +20,8 @@ public class PortalableObject : MonoBehaviour
 
     protected virtual void Awake()
     {
-        cloneObject = new GameObject();
-        cloneObject.SetActive(false);
-        var meshFilter = cloneObject.AddComponent<MeshFilter>();
-        var meshRenderer = cloneObject.AddComponent<MeshRenderer>();
-
-        meshFilter.mesh = GetComponent<MeshFilter>().mesh;
-        meshRenderer.materials = GetComponent<MeshRenderer>().materials;
-        cloneObject.transform.localScale = transform.localScale;
-
         rigidbody = GetComponent<Rigidbody>();
         collider = GetComponent<Collider>();
-    }
-
-    private void LateUpdate()
-    {
-        if(inPortal == null || outPortal == null)
-        {
-            return;
-        }
-
-        if(cloneObject.activeSelf && inPortal.IsPlaced && outPortal.IsPlaced)
-        {
-            var inTransform = inPortal.transform;
-            var outTransform = outPortal.transform;
-
-            // Update position of clone.
-            Vector3 relativePos = inTransform.InverseTransformPoint(transform.position);
-            relativePos = halfTurn * relativePos;
-            cloneObject.transform.position = outTransform.TransformPoint(relativePos);
-
-            // Update rotation of clone.
-            Quaternion relativeRot = Quaternion.Inverse(inTransform.rotation) * transform.rotation;
-            relativeRot = halfTurn * relativeRot;
-            cloneObject.transform.rotation = outTransform.rotation * relativeRot;
-        }
-        else
-        {
-            cloneObject.transform.position = new Vector3(-1000.0f, 1000.0f, -1000.0f);
-        }
     }
 
     public void SetIsInPortal(Portal inPortal, Portal outPortal, Collider wallCollider)
@@ -69,46 +30,37 @@ public class PortalableObject : MonoBehaviour
         this.outPortal = outPortal;
 
         Physics.IgnoreCollision(collider, wallCollider);
-
-        cloneObject.SetActive(false);
-
-        ++inPortalCount;
     }
 
     public void ExitPortal(Collider wallCollider)
     {
         Physics.IgnoreCollision(collider, wallCollider, false);
-        --inPortalCount;
-
-        if (inPortalCount == 0)
-        {
-            cloneObject.SetActive(false);
-        }
     }
 
     public virtual void Warp()
     {
         var inTransform = inPortal.transform;
         var outTransform = outPortal.transform;
-
+        
+        // Teleport object to the portal's center.
+        transform.position = inTransform.position + new Vector3(0f, 0.01f, 0f);
+        
         // Update position of object.
         Vector3 relativePos = inTransform.InverseTransformPoint(transform.position);
         relativePos = halfTurn * relativePos;
         transform.position = outTransform.TransformPoint(relativePos);
-
+        
         // Update rotation of object.
         Quaternion relativeRot = Quaternion.Inverse(inTransform.rotation) * transform.rotation;
         relativeRot = halfTurn * relativeRot;
         transform.rotation = outTransform.rotation * relativeRot;
-
+        
         // Update velocity of rigidbody.
         Vector3 relativeVel = inTransform.InverseTransformDirection(rigidbody.velocity);
         relativeVel = halfTurn * relativeVel;
         rigidbody.velocity = outTransform.TransformDirection(relativeVel);
-
+        
         // Swap portal references.
-        var tmp = inPortal;
-        inPortal = outPortal;
-        outPortal = tmp;
+        (inPortal, outPortal) = (outPortal, inPortal);
     }
 }
