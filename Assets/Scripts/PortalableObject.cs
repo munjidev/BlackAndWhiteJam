938cbed8f5453764f23ec29163200d943c61a0b9
munij-dev <muniainjuan@icloud.com>
2023-02-10 20:56:19 -0600

    Modify portalable object script
    Modified transform correction when entering second portal, renamed variables.

--- Assets/Scripts/PortalableObject.cs
+++ Assets/Scripts/PortalableObject.cs
@@ -8,59 +8,65 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class PortalableObject : MonoBehaviour
 {
-    private GameObject cloneObject;
-
-    private Portal inPortal;
-    private Portal outPortal;
-
-    private new Rigidbody rigidbody;
-    protected new Collider collider;
-
+    protected Collider _collider;
+    
     private static readonly Quaternion halfTurn = Quaternion.Euler(0.0f, 180.0f, 0.0f);
+    
+    private Portal _inPortal;
+    private Portal _outPortal;
+    private Rigidbody _rigidbody;
 
     protected virtual void Awake()
     {
-        rigidbody = GetComponent<Rigidbody>();
-        collider = GetComponent<Collider>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _collider = GetComponent<Collider>();
     }
 
     public void SetIsInPortal(Portal inPortal, Portal outPortal, Collider wallCollider)
     {
-        this.inPortal = inPortal;
-        this.outPortal = outPortal;
+        _inPortal = inPortal;
+        _outPortal = outPortal;
 
-        Physics.IgnoreCollision(collider, wallCollider);
+        Physics.IgnoreCollision(_collider, wallCollider);
     }
 
     public void ExitPortal(Collider wallCollider)
     {
-        Physics.IgnoreCollision(collider, wallCollider, false);
+        Physics.IgnoreCollision(_collider, wallCollider, false);
     }
 
     public virtual void Warp()
     {
-        var inTransform = inPortal.transform;
-        var outTransform = outPortal.transform;
+        Transform inTransform = _inPortal.transform;
+        Transform outTransform = _outPortal.transform;
         
         // Teleport object to the portal's center.
-        transform.position = inTransform.position + new Vector3(0f, 0.01f, 0f);
-        
+        if (_inPortal.name == "Portal 2")
+        {
+            transform.position = inTransform.position + new Vector3(0f, 0.1f, 0f);
+        }
+
         // Update position of object.
         Vector3 relativePos = inTransform.InverseTransformPoint(transform.position);
         relativePos = halfTurn * relativePos;
         transform.position = outTransform.TransformPoint(relativePos);
-        
+
         // Update rotation of object.
         Quaternion relativeRot = Quaternion.Inverse(inTransform.rotation) * transform.rotation;
-        relativeRot = halfTurn * relativeRot;
+        
+        // If the object is upside down, rotate it 180 degrees around the x and z axes.
+        if (relativeRot.eulerAngles is { x: 180f, z: 180f })
+        {
+            relativeRot = halfTurn * relativeRot;
+        }
         transform.rotation = outTransform.rotation * relativeRot;
         
         // Update velocity of rigidbody.
-        Vector3 relativeVel = inTransform.InverseTransformDirection(rigidbody.velocity);
+        Vector3 relativeVel = inTransform.InverseTransformDirection(_rigidbody.velocity);
         relativeVel = halfTurn * relativeVel;
-        rigidbody.velocity = outTransform.TransformDirection(relativeVel);
-        
+        _rigidbody.velocity = outTransform.TransformDirection(relativeVel);
+
         // Swap portal references.
-        (inPortal, outPortal) = (outPortal, inPortal);
+        (_inPortal, _outPortal) = (_outPortal, _inPortal);
     }
 }
