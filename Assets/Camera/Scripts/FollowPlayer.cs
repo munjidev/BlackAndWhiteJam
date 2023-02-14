461e896d55d294935c52a423fafdbcabc20e195d
munij-dev <muniainjuan@icloud.com>
2023-02-14 15:17:22 -0600

    Add camera follow coroutine for linear interpolation

--- Assets/Camera/Scripts/FollowPlayer.cs
+++ Assets/Camera/Scripts/FollowPlayer.cs
@@ -10,13 +10,13 @@ public class FollowPlayer : MonoBehaviour
     {
         if (_moving) return;
         
-        if (player.position.y < -offset.y || player.position.y >= 13f) return;
+        if (player.position.y < -offset.y || player.position.y >= 10f) return;
 
         Vector3 pos = transform.position;
         pos.y = player.position.y + offset.y;
         Vector3 startPos = transform.position;
-        StartCoroutine(MoveCamera(startPos, pos, 0.01f));
         _moving = true;
+        StartCoroutine(MoveCamera(startPos, pos, 0.01f));
     }
     
     // Coroutine to lerp the player to the new position
@@ -27,8 +27,8 @@ public class FollowPlayer : MonoBehaviour
         {
             t += Time.deltaTime / time;
             transform.position = Vector3.Lerp(startPos, endPos, t);
-            _moving = false;
             yield return null;
         }
+        _moving = false;
     }
 }
